80d5b22591d39612a926ad8743a98cb70d72e965
roshanmarakana <roshanmarakana77@gmail.com>
2025-08-19 22:39:51 +0530

    Add files via upload

---
+++ convert dollers into Rs.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,Rs;
+    clrscr();
+    printf("Enter the dollers:");
+    scanf("%d",&a);
+    Rs=a*75;
+    printf("This given dollers into Rs is %d * %d = %d\n",a,75,Rs);
+    getch();
+
+}
