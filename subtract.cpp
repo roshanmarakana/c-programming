6a965676caecdae2a2ed3c1b69e111abb09ed6c8
roshanmarakana <roshanmarakana77@gmail.com>
2025-08-19 20:42:14 +0530

    Add files via upload

---
+++ subtract.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter two numbers:");
+    scanf("%d%d",&a,&b);
+    c=a-b;
+    printf("%d - %d = %d",a,b,c);
+    getch();
+}
